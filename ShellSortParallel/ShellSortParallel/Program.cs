365d817d2d2a1267e44be5cccced4c5b3d44814f
Eduard <circeag.eduard@gmail.com>
2017-05-18 22:06:25 +0300

    Added ShellSort threaded

---
+++ ShellSortParallel/ShellSortParallel/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShellSortParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Thread> threads = new List<Thread>();
+            int N;
+            int[] A = {1,3,2,12,6,30,15,11,23};
+            N = A.Length;
+            int h;
+            h = N / 2;
+            ShellSort sort = new ShellSort();
+            while (h > 0)
+            {
+                for (int k = 0; k < h; k++)
+                {
+                    Thread t = new Thread(() => sort.shellSort(k,h,N,A));
+                    t.Start();
+                    threads.Add(t);
+                }
+
+                foreach (var thread in threads)
+                    thread.Join();
+
+                h = h / 2;
+            }
+
+            for(int i = 0; i < N ; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ShellSortParallel/ShellSortParallel/ShellSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShellSortParallel
+{
+    public class ShellSort
+    {
+        public void shellSort(int k, int h, int N, int[] A) {
+            int i, j, v;
+            for (i = k; i + h < N; i++)
+            {
+                j = i + h;
+                v = A[j];
+
+                while (j - h >= 0 && v < A[j - h])
+                {
+                    A[j] = A[j - h];
+                    j = j - h;
+                }
+
+                A[j] = v;
+            }
+
+        }
+    }
+}
